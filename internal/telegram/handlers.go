421faf274dff8033e7f8fa863abfa53f60c72fc2
Mykhailo Ponomarenko <mishaponomarenko11082001@gmail.com>
2026-03-26 11:23:36 +0200

    Fixed bot commands.

--- internal/telegram/handlers.go
+++ internal/telegram/handlers.go
@@ -3,6 +3,7 @@ package telegram
 import (
 	"log"
 	"os/exec"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
@@ -20,14 +21,13 @@ func handleMessage(bot *tgbotapi.BotAPI, cfg config.Config, msg *tgbotapi.Messag
 	}
 
 	switch msg.Command() {
-
 	case "poweroff":
 		send(bot, msg.Chat.ID, "⚠️ Вимикаю систему...")
-		go runCommand("sudo", "poweroff", "now")
+		go runCommand("sudo", "systemctl", "poweroff")
 
 	case "reboot":
 		send(bot, msg.Chat.ID, "🔄 Перезавантажую...")
-		go runCommand("sudo", "reboot")
+		go runCommand("sudo", "systemctl", "reboot")
 
 	case "ping":
 		send(bot, msg.Chat.ID, "pong 🏓")
@@ -44,7 +44,12 @@ func send(bot *tgbotapi.BotAPI, chatID int64, text string) {
 
 func runCommand(name string, args ...string) {
 	cmd := exec.Command(name, args...)
-	if err := cmd.Run(); err != nil {
-		log.Println("command error:", err)
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Printf("command error: %v, output: %s", err, strings.TrimSpace(string(output)))
+		return
 	}
+
+	log.Printf("command success: %s %s", name, strings.Join(args, " "))
 }
